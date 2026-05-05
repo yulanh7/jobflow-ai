c9f7e2d06deab3e7e59d507c07bcba9b8978ed22
Yulan Huang <yulanh.tech@gmail.com>
2026-05-05 15:21:09 +1000

    chore: update metadata and standardise code comments to English

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,7 +2,8 @@ import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 /**
- * 合并 Tailwind 类名的标准工具函数
+ * Merges Tailwind CSS class names, resolving conflicts with tailwind-merge
+ * and handling conditional classes with clsx.
  */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
